fb76993e0a5af69f880fdc76c4989f935beb6214
Herbert Arias <herbert.arsilva@gmail.com>
2022-06-25 04:47:41 -0500

    feat: postgresql connection

--- rescatadog/settings.py
+++ rescatadog/settings.py
@@ -14,7 +14,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
 ]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'rescatadog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rescatadog',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     },
 }
 
