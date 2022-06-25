a4516e3763204f917f61d31990237e3ded26c615
SANTIAGOBCF <58187994+SANTIAGOBCF@users.noreply.github.com>
2022-06-25 12:38:31 -0500

    admin adoptions

--- adoptions/admin.py
+++ adoptions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from adoptions.models import Adoption, AdoptionDetail
+
 # Register your models here.
+
+class AdoptionAdmin(admin.ModelAdmin):
+    pass
+class AdoptionDetailAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Adoption, AdoptionAdmin)
+admin.site.register(AdoptionDetail, AdoptionDetailAdmin)
