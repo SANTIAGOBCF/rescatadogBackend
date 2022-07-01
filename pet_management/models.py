0fb667767b6e79d06251f850dcbc4af061a995e1
Herbert Arias <herbert.arsilva@gmail.com>
2022-07-01 15:23:53 -0500

    Update pet models

--- pet_management/models.py
+++ pet_management/models.py
@@ -1,4 +1,21 @@
+from datetime import datetime
 from django.db import models
+
+
+class PetCategory(models.Model):
+    name = models.CharField(max_length=180)
+    created_at=models.DateTimeField(auto_now=datetime.now)
+    updated_at=models.DateTimeField()
+    deleted=models.BooleanField(auto_created=False)
+
+
+class PetProfile(models.Model):
+    gender = models.CharField(max_length=60)
+    age = models.IntegerField()
+    location = models.CharField(max_length=250)
+    created_at=models.DateTimeField(auto_now=datetime.now)
+    updated_at=models.DateTimeField()
+    deleted=models.BooleanField(auto_created=False)
 
 
 class Pet(models.Model):
@@ -6,3 +23,14 @@ class Pet(models.Model):
     description = models.TextField()
     url = models.URLField(null=True)
     is_adopted = models.BooleanField(default=False)
+    pet_category = models.ForeignKey(
+        PetCategory,
+        on_delete=models.CASCADE
+    )
+    pet_profile = models.ForeignKey(
+        PetProfile,
+        on_delete=models.CASCADE
+    )
+    created_at=models.DateTimeField(auto_now=datetime.now)
+    updated_at=models.DateTimeField()
+    deleted=models.BooleanField(auto_created=False)
