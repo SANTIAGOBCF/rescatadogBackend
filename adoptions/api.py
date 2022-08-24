c0c0dfcc8d2b6f3f1c083f22d75db70201e90fc0
SANTIAGOBCF <58187994+SANTIAGOBCF@users.noreply.github.com>
2022-08-24 18:46:40 -0500

    more endpoints(adoptions)

--- adoptions/api.py
+++ adoptions/api.py
@@ -1,11 +1,27 @@
 from adoptions.models import *
 from ninja import Router
 
-from adoptions.schema import CreateAdoptionSchema
+from adoptions.schema import AdoptionAllSchema, CreateAdoptionSchema
 
 router = Router()
 
-@router.post('/',auth=None, summary="Register Adoption",description="Fill" )
+@router.post('/', summary="Register Adoption",description="Fill" )
 def registerAdoption(request,payload: CreateAdoptionSchema):
     Adoption.objects.create(**payload.dict())
     return {"message":"Adoption registered"}
+
+@router.get('/', summary="Get all adoptions",description="Just click",response=list[AdoptionAllSchema] )
+def getAllAdoptions(request):
+    return Adoption.objects.all()
+
+@router.get('/petId/{petId}',response=list[AdoptionAllSchema] )
+def getAllAdoptionsByPetId(request, petId: int):
+    return Adoption.objects.filter(pet_id=petId)
+
+@router.get('/adopterId/{adopterId}',response=list[AdoptionAllSchema] )
+def getAllAdoptionsByAdopterId(request, adopterId: int):
+    return Adoption.objects.filter(adopter_id=adopterId)
+
+@router.get('/rescuerId/{rescuerId}',response=list[AdoptionAllSchema] )
+def getAllAdoptionsByRescuerId(request, rescuerId: int):
+    return Adoption.objects.filter(rescuer_id=rescuerId)
